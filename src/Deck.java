09d374e001b4dbb9747f392e45e26ea241f8f922
Sonia V Mathew <sonisvm@gmail.com>
2015-07-31 08:53:40 +0530

    added function definitions to the classes

--- src/Deck.java
+++ src/Deck.java
@@ -3,4 +3,8 @@ public class Deck {
 		public static Card[] getNewHand(){
 			
 		}
+		
+		public static void main(String args[]){
+			
+		}
 }
--- src/Hand.java
+++ src/Hand.java
@@ -1,9 +1,36 @@
 
 public class Hand {
-	Card cards[];
+	Card cardsInHand[];
 	
 	Hand(){
-		cards = Deck.getNewHand();
+		cardsInHand = Deck.getNewHand();
 	}
 	
+	public boolean isSequence(Card cards[]){
+		
+	}
+	
+	public boolean isSet(Card cards[]){
+		
+	}
+	
+	public boolean isCanasta(Card cards[]){
+		
+	}
+	
+	public boolean isPossibleSequence(Card cards[]){
+		
+	}
+	
+	public boolean isPossibleSet(Card cards[]){
+		
+	}
+	
+	public void removeCards(Card cards[]){
+		
+	}
+	
+	public void countHand(Card cards[]){
+		
+	}
 }
