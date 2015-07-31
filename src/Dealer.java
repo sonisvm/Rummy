3f8691c53e48c385e489902b16debf3943962bdc
Sonia V Mathew <sonisvm@gmail.com>
2015-07-31 09:13:39 +0530

    added function to randomly generate cards

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,10 +1,21 @@
+import java.util.Random;
 
 public class Dealer {
 		public static Card[] getNewHand(){
-			
+			Random r = new Random();
+			Card[] hand = new Card[13];
+			int num;
+			int High = 156;
+			int Low = 1;
+			for(int i=0;i<13;i++){
+				num = r.nextInt(High-Low) + Low;
+				hand[i] = getCard(num);
+			}
+			 return hand;
 		}
 		
 		public static void main(String args[]){
-			Deck
+			getNewHand();
 		}
+		
 }
