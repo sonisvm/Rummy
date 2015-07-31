b54c436e79ffb8b50ed7ff65f7cde811bff68a30
jayasruthi <jayasruthivr@gmail.com>
2015-07-31 08:56:42 +0530

    Card class

---
+++ Card.java
@@ -0,0 +1,37 @@
+package Rummy;
+
+public class Card {		
+	private	int suit;
+	private int rank;
+	
+	public Card(int rank,int suit)
+	{
+		this.rank = rank;
+		this.suit = suit;
+	}
+
+	public int getSuit() {
+		return suit;
+	}
+	public void setSuit(int suit) {
+		this.suit = suit;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	
+	public Card CompareCardBySuit(Card c1)
+	{
+		if (this.suit > c1.suit) return this;
+		else return c1;
+	}
+	
+	public Card CompareCardByRank(Card c1)
+	{
+		if (this.rank > c1.rank) return this;
+		else return c1;
+	}
+}
